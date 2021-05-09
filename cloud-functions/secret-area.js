24d6cf3356ec74d48bb78481091e1e4678e8a5d3
Scott Powers <spowers2@me.com>
2021-05-08 22:09:34 -0400

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,30 @@
 exports.handler = function(event, context, callback){
+    const secretContent =  `
+    <h3>Welcome to the Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong> and 2+2 = 4
+    `
+
+    let body
+if (event.body){
+    body.JSON.parse(event.body)
+}else{
+    body = {
+
+    }
+}
+
+if (body.password == "javascript"){
+    callback(null, {
+        statusCode: 200,
+        body: secretContent
+    })
+} else {
+    callback(null, {
+        statusCode: 200,
+        body: "Welcome to the Super Secret Area"
+    })
+}
+
     callback(null, {
         statusCode: 200,
         body: "Welcome to the Super Secret Area"
