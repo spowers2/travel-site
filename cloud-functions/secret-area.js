110b72508adc46ec2641808ade7353acd5413899
Scott Powers <spowers2@me.com>
2021-05-09 21:02:17 -0400

    Updated cloud function with CORS policy.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,24 +1,42 @@
-exports.handler = function(event, context, callback){
-    const secretContent =  `
-    <h3>Welcome to the Secret Area</h3>
-    <p>Here we can tell you that the sky is <strong>blue</strong> and 2+2 = 4
-    `
-
-    let body
-if (event.body){
-    body = JSON.parse(event.body)
-}else{
-    body = {}
-}
-
-if (body.password == "javascript"){
-    callback(null, {
+exports.handler = function(event, context, callback) {
+    const headers = {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Headers" : "Content-Type"
+    }
+   
+    if (event.httpMethod !== "POST") {
+      return callback(null, {
         statusCode: 200,
+        headers,
+        body: "This was not a POST request"
+      })
+    }
+    
+    const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `
+    
+    let body
+   
+    if (event.body) {
+      body = JSON.parse(event.body)
+    } else {
+      body = {}
+    }
+   
+    if (body.password == "javascript") {
+      callback(null, {
+        statusCode: 200,
+        headers,
         body: secretContent
-    })
-} else {
-    callback(null, {
-        statusCode: 401
-    })
-}
-}
+      })
+    } else {
+      callback(null, {
+        statusCode: 401,
+        headers
+      })
+    }
+   
+    
+  }
